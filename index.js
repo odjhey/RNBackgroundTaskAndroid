085045fc49e538584029e279c7ae6afc14ed7ac9
odjhey <odjhey@gmail.com>
2023-05-14 11:12:44 +0800

    feat: setup headless receiver

--- index.js
+++ index.js
@@ -6,4 +6,8 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
+AppRegistry.registerHeadlessTask('SomeTaskHere', () => {
+  console.log('hello world - received');
+});
+
 AppRegistry.registerComponent(appName, () => App);
